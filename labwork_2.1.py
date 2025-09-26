e145f5b3ea3bb65b14e50068a4cb97d3bdffe1b1
kindheadbanger <viperz946@gmail.com>
2025-09-26 13:59:17 +0300

    labwork_2.1

---
+++ labwork_2.1.py
@@ -0,0 +1,22 @@
+import os
+import stat
+
+def permissions(path: str, mode: int):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            file_path = os.path.join(root, name)
+            try:
+                os.chmod(file_path, mode)
+                print(f"success: {file_path}")
+            except Exception as ex:
+                print(f"error {file_path}: {ex}")
+
+        for name in dirs:
+            dir_path = os.path.join(root, name)
+            try:
+                os.chmod(dir_path, mode)
+                print(f"success: {dir_path}")
+            except Exception as ex:
+                print(f"error {dir_path}: {ex}")
+
+permissions("D:/Files/education/Created/test", 0o644)
